794d66ead03f906ff53f0d16a088b80602fb05e7
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-03 16:33:38 -0700

    styling for images

--- src/components/OrderImages.js
+++ src/components/OrderImages.js
@@ -3,9 +3,15 @@ import React from 'react';
 export default function OrderImages({ foodId, sideId, drinkId }) {
   return (
     <div className="order-images">
-      <img alt="food" src={`food-${foodId}.png`} />
-      <img alt="side" src={`side-${sideId}.png`} />
-      <img alt="drink" src={`drink-${drinkId}.png`} />
+      <div className="image-container">
+        <img alt="food" src={`food-${foodId}.png`} />
+      </div>
+      <div className="image-container">
+        <img alt="side" src={`side-${sideId}.png`} />
+      </div>
+      <div className="image-container">
+        <img alt="drink" src={`drink-${drinkId}.png`} />
+      </div>
     </div>
   );
 }
