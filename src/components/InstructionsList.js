04dab0437718ee6ca08115ce496817afd44abe4b
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-03 16:16:28 -0700

    InstructionsList accepts instructions as a prop. InstructionsList maps over instructions and renders an Instruction component (which is passed instruction as a prop) for each item in the instructions array

--- src/components/InstructionsList.js
+++ src/components/InstructionsList.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import Instruction from './Instruction';
 
-export default function InstructionsList() {
-  return <div>InstructionsList</div>;
+export default function InstructionsList({ instructions }) {
+  return (
+    <div className="instructions">
+      <h5>Instructions:</h5>
+      {instructions.map((instruction, i) => (
+        <Instruction key={instruction + i} instruction={instruction} />
+      ))}
+    </div>
+  );
 }
