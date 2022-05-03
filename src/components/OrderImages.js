93b6bda9211700afb25e12b6adbb6ed56ee7b646
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-03 16:22:48 -0700

    added image files to public - copied from assignment repo. added .png extension to src property of the img tags. rendering the images correctly

--- src/components/OrderImages.js
+++ src/components/OrderImages.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function OrderImages({ foodId, sideId, drinkId }) {
   return (
     <div className="order-images">
-      <img alt="food" src={`food-${foodId}`} />
-      <img alt="side" src={`side-${sideId}`} />
-      <img alt="drink" src={`drink-${drinkId}`} />
+      <img alt="food" src={`food-${foodId}.png`} />
+      <img alt="side" src={`side-${sideId}.png`} />
+      <img alt="drink" src={`drink-${drinkId}.png`} />
     </div>
   );
 }
