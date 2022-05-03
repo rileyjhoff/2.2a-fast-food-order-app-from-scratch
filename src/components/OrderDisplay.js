4a66a12ebc98a2c567a463346057d851f719a37d
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-03 16:09:50 -0700

    OrderDisplay accepts orderName, foodId, sideId, drinkId, and instructions as props. OrderDisplay calls OrderImages and passes it foodId, sideId, and drinkId as props, and calls InstructionsList and passes it instructions as a prop

--- src/components/OrderDisplay.js
+++ src/components/OrderDisplay.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import InstructionsList from './InstructionsList';
+import OrderImages from './OrderImages';
 
-export default function OrderDisplay() {
-  return <div>OrderDisplay</div>;
+export default function OrderDisplay({ orderName, foodId, sideId, drinkId, instructions }) {
+  return (
+    <div className="order-display">
+      <h3>Order for {orderName}</h3>
+      <OrderImages foodId={foodId} sideId={sideId} drinkId={drinkId} />
+      <InstructionsList instructions={instructions} />
+    </div>
+  );
 }
