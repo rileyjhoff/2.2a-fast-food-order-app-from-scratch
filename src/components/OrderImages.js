0780b16f0d91866239371711bb6e710be16caefe
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-03 16:12:36 -0700

    OrderImages accepts foodId, sideId, and drinkId as props. OrderImages returns a div with three image tags - one for food, drink, and a side, with each image depending on the value of the props

--- src/components/OrderImages.js
+++ src/components/OrderImages.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-export default function OrderImages() {
-  return <div>OrderImages</div>;
+export default function OrderImages({ foodId, sideId, drinkId }) {
+  return (
+    <div className="order-images">
+      <img alt="food" src={`food-${foodId}`} />
+      <img alt="side" src={`side-${sideId}`} />
+      <img alt="drink" src={`drink-${drinkId}`} />
+    </div>
+  );
 }
