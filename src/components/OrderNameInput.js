843d7908a9622bcca1a202976afb3328bcbe5bd0
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-03 17:29:08 -0700

    OrderNameInput accepts setOrderName as a prop. It returns a div with some text and an input. Whenever the input value changes, setOrderName is calledand order name is updated in the DOM

--- src/components/OrderNameInput.js
+++ src/components/OrderNameInput.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export default function OrderNameInput() {
-  return <div>OrderNameInput</div>;
+export default function OrderNameInput({ setOrderName }) {
+  return (
+    <div className="order-name">
+      Who is this order for?
+      <input onChange={(e) => setOrderName(e.target.value)} />
+    </div>
+  );
 }
