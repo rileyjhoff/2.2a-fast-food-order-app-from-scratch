a51dd65b1954434b04aa5fed0fbba24a7299cfee
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-03 17:52:36 -0700

    some styling for instruction form

--- src/components/InstructionForm.js
+++ src/components/InstructionForm.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 export default function InstructionForm({ handleSubmit, instructionInput, setInstructionInput }) {
   return (
-    <div>
+    <div className="instruction-form">
+      Add a special instruction:
       <form onSubmit={handleSubmit}>
         <input value={instructionInput} onChange={(e) => setInstructionInput(e.target.value)} />
         <button>Submit</button>
