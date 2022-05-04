f4e9443ab74498929e627b1eca85ef2c75419fe8
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-03 18:08:24 -0700

    update test to render OrderDisplay instead of App since there is no text in App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import OrderDisplay from './components/OrderDisplay';
 
 test('renders Order for text', () => {
-  render(<App />);
+  render(<OrderDisplay />);
   const textElement = screen.getByText(/Order for/i);
   expect(textElement).toBeInTheDocument();
 });
