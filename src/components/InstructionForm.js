056f3b6bf529d7e47b25e9294ec2b6e645e465e5
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-03 17:43:42 -0700

    InstructionForm accepts handleSubmit, instructionInput, and setInstructionInput as props. On form submit, handleSubmit is called. Input value is set equal to instructionInput, and on input change, setInstructionInput is called to update instructionInput to whatever the user is typing

--- src/components/InstructionForm.js
+++ src/components/InstructionForm.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export default function InstructionForm() {
-  return <div></div>;
+export default function InstructionForm({ handleSubmit, instructionInput, setInstructionInput }) {
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input value={instructionInput} onChange={(e) => setInstructionInput(e.target.value)} />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
