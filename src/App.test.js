a9e9a03a2436c980de7bcd43db6f49cdea52220f
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-03 18:05:37 -0700

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Order for text', () => {
   render(<App />);
   const textElement = screen.getByText(/Order for/i);
   expect(textElement).toBeInTheDocument();
